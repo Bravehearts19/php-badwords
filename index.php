8e5cc0c65eb7cc64c7b9fd47e33b6f3cb377f58f
Bravehearts19 <leo18111998@gmail.com>
2021-11-29 09:40:42 +0100

    Exercise uploaded

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+$text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum esse perferendis animi laboriosam distinctio sapiente ipsum totam eius sint, delectus eum possimus! Aliquid sed excepturi illo cupiditate nostrum necessitatibus sint.";
+$textLength = strlen($text);
+$badWord = $_GET("badWord");
+$textToPrint = str_replace($badWord, "***", $text)
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Php Badword</title>
+    </head>
+
+    <body>
+        <header></header>
+        <main>
+            <p>
+                <?php echo $textToPrint ?> <br>
+                <?php echo $textLength ?>
+            </p>
+        </main>
+        <footer></footer>
+    </body>
+</html>
+
